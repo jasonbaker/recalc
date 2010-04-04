1ce3edb023acbcd9b893074b35a25c23abc1c8c4
Jason Baker <amnorvend@gmail.com>
2010-04-04 11:13:04 -0500

    Formatting this so it's actually human-editable

--- project.clj
+++ project.clj
@@ -1,1 +1,6 @@
-(defproject recalc "1.0.0-SNAPSHOT" :description "FIXME: write" :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"] [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+(defproject recalc "1.0.0-SNAPSHOT" 
+  :description "An implementation of relational calculus" 
+  :dependencies [[org.clojure/clojure "1.1.0"] 
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.clojars.hiredman/fnparse "2.2.4"]]
+  :main recalc)
