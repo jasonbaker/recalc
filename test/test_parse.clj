3af6a7c9f3072678a5ceff7db134214a3a05b720
Jason Baker <amnorvend@gmail.com>
2010-04-08 10:46:42 -0500

    adding kind tests for number

--- test/test_parse.clj
+++ test/test_parse.clj
@@ -6,7 +6,9 @@
   (is (= (:content (parse int-lit "12")) 12))
   (is (= (:content (parse float-lit "12.2")) 12.2))
   (is (= (:content (parse number "12")) 12))
-  (is (= (:content (parse number "12.2")) 12.2)))
+  (is (= (:content (parse number "12.2")) 12.2))
+  (is (= (:kind (parse number "12")) :scalar))
+  (is (= (:kind (parse number "12.2")) :scalar)))
   
 (deftest letter_test
   (is (= (parse letter "a") \a))
